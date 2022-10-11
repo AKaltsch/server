a71c7ec45ce9d8be2d2379c059705459ac81bece
Alec Kaltschmid <kaltschmidalec@gmail.com>
2022-10-10 22:25:10 -0700

    MongoDB connection set up

--- App.js
+++ App.js
@@ -1,17 +1,33 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const app = express();
 
-//test save
+const MONGODB_URI = process.env.MONGODB_KEY;
 
 app.use(cors());
 app.use(express.json());
+
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
 
 app.post("/api/register", (req, res, next) => {
   console.log(req.body);
 });
 
-app.listen(4000, () => {
-  console.log("server is running on port 4000");
-});
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    app.listen(4000, () => {
+      console.log("Server is running on port 4000");
+    });
+  })
+  .catch((err) => console.log(err));
