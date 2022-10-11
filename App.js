b83cd5b18cb29eef9483b7eebb9277cc02e0ae14
Alec Kaltschmid <kaltschmidalec@gmail.com>
2022-10-10 22:59:29 -0700

    register and login routes created

--- App.js
+++ App.js
@@ -9,6 +9,8 @@ const MongoDBStore = require("connect-mongodb-session")(session);
 
 const app = express();
 
+const User = require("./Models/UserModel");
+
 const MONGODB_URI = process.env.MONGODB_KEY;
 
 app.use(cors());
@@ -19,8 +21,29 @@ const store = new MongoDBStore({
   collection: "sessions",
 });
 
-app.post("/api/register", (req, res, next) => {
-  console.log(req.body);
+app.post("/api/register", async (req, res, next) => {
+  try {
+    await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.json({ status: "ok" });
+  } catch (err) {
+    res.json({ status: "error", error: "duplicate name/email" });
+  }
+});
+
+app.post("/api/login", async (req, res, next) => {
+  const user = await User.findOne({
+    email: req.body.emaild,
+    password: req.body.password,
+  });
+  if (user) {
+    res.json({ status: "ok", user: true });
+  } else {
+    res.json({ status: "error", user: false });
+  }
 });
 
 mongoose
--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
-const User = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
     quote: { type: String },
-  },
-  { collection: "user-data" }
+  }
+  //   { collection: "user-data" }
 );
 
-const model = mongoose.model("UserData", User);
+const model = mongoose.model("User", userSchema);
 
 module.exports = model;
