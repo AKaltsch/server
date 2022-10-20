ce43a3861e4868dcc8171fd8f5f74603fdaef41f
Alec Kaltschmid <kaltschmidalec@gmail.com>
2022-10-19 21:43:54 -0700

    auth and quote routes created

--- App.js
+++ App.js
@@ -56,6 +56,39 @@ app.post("/api/login", async (req, res, next) => {
   }
 });
 
+app.get("/api/quote", async (req, res, next) => {
+  const token = req.headers["x-access-token"];
+
+  try {
+    const decoded = jwt.verify(token, SECRET);
+    const email = decoded.email;
+    const user = await User.findOne({ email: email });
+
+    return res.json({ status: "ok", quote: user.quote });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", error: "invalid token" });
+  }
+});
+
+app.post("/api/quote", async (req, res, next) => {
+  const token = req.headers["x-access-token"];
+
+  try {
+    const decoded = jwt.verify(token, SECRET);
+    const email = decoded.email;
+    const user = await User.updateOne(
+      { email: email },
+      { $set: { quote: req.body.quote } }
+    );
+
+    return res.json({ status: "ok" });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", error: "invalid token" });
+  }
+});
+
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
