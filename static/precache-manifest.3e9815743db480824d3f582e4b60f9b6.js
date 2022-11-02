77225e162fa33fb080ad6e9847642f36ca8f6f43
Alec Kaltschmid <kaltschmidalec@gmail.com>
2022-11-02 10:58:37 -0700

    build folder added as static

---
+++ static/precache-manifest.3e9815743db480824d3f582e4b60f9b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0625619f5bcbc52e870f",
+    "url": "/static/css/main.33165c14.chunk.css"
+  },
+  {
+    "revision": "0625619f5bcbc52e870f",
+    "url": "/static/js/main.d97a1ccc.chunk.js"
+  },
+  {
+    "revision": "b29d8a849139f64ab4ee",
+    "url": "/static/js/runtime~main.c58ff8e6.js"
+  },
+  {
+    "revision": "a1a9e3cf9295b7f2f875",
+    "url": "/static/js/2.ffa081be.chunk.js"
+  },
+  {
+    "revision": "408621d3a15eaf977c3c",
+    "url": "/static/js/3.87f4fc67.chunk.js"
+  },
+  {
+    "revision": "4974130d86cf713aa81c2f2a83e78896",
+    "url": "/index.html"
+  }
+];
